978d49091a3fdc0284d8d31b481f18e94fc42aeb
soroushby <49848393+soroushby@users.noreply.github.com>
2020-07-22 03:46:55 -0700

    added angular material

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -3,16 +3,26 @@ import { NgModule } from '@angular/core';
 
 import { AppRoutingModule } from './app-routing.module';
 import { AppComponent } from './app.component';
+import { AngularFireModule } from '@angular/fire';
+import { environment } from 'src/environments/environment';
+
+import { AngularFirestoreModule } from '@angular/fire/firestore';
+import { AngularFireStorageModule } from '@angular/fire/storage';
+import { AngularFireAuthModule } from '@angular/fire/auth';
+import { BrowserAnimationsModule } from '@angular/platform-browser/animations';
 
 @NgModule({
-  declarations: [
-    AppComponent
-  ],
+  declarations: [AppComponent],
   imports: [
     BrowserModule,
-    AppRoutingModule
+    AppRoutingModule,
+    AngularFireModule.initializeApp(environment.firebaseConfig, 'matabProject'),
+    AngularFirestoreModule,
+    AngularFireStorageModule,
+    AngularFireAuthModule,
+    BrowserAnimationsModule,
   ],
   providers: [],
-  bootstrap: [AppComponent]
+  bootstrap: [AppComponent],
 })
-export class AppModule { }
+export class AppModule {}
--- src/environments/environment.ts
+++ src/environments/environment.ts
@@ -3,7 +3,17 @@
 // The list of file replacements can be found in `angular.json`.
 
 export const environment = {
-  production: false
+  production: false,
+  firebaseConfig: {
+    apiKey: 'AIzaSyAVV4JYUme3uPdZa2NG94g8ADrI5JCntBw',
+    authDomain: 'matabproject.firebaseapp.com',
+    databaseURL: 'https://matabproject.firebaseio.com',
+    projectId: 'matabproject',
+    storageBucket: 'matabproject.appspot.com',
+    messagingSenderId: '841252185715',
+    appId: '1:841252185715:web:1088a0bcb40af1a590ec06',
+    measurementId: 'G-VQ4S1B77VF',
+  },
 };
 
 /*
