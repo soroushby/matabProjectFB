8647a181fa5c36699e432b4c10535903958fb556
soroushby <49848393+soroushby@users.noreply.github.com>
2020-07-28 02:35:58 -0700

    stated to go to production

--- src/environments/environment.prod.ts
+++ src/environments/environment.prod.ts
@@ -1,3 +1,13 @@
 export const environment = {
-  production: true
+  production: true,
+  firebaseConfig: {
+    apiKey: 'AIzaSyAVV4JYUme3uPdZa2NG94g8ADrI5JCntBw',
+    authDomain: 'matabproject.firebaseapp.com',
+    databaseURL: 'https://matabproject.firebaseio.com',
+    projectId: 'matabproject',
+    storageBucket: 'matabproject.appspot.com',
+    messagingSenderId: '841252185715',
+    appId: '1:841252185715:web:1088a0bcb40af1a590ec06',
+    measurementId: 'G-VQ4S1B77VF',
+  },
 };
